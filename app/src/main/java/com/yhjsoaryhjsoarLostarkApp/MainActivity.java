f2c3bcd596858b2caa7034749a687c0ae52bc183
lezin <yhjsoar5@gmail.com>
2019-05-19 14:26:53 +0900

    Initial commit

---
+++ app/src/main/java/com/yhjsoaryhjsoarLostarkApp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.yhjsoaryhjsoarLostarkApp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final String[] LIST_MENU = {"섬의 마음 수집"} ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU) ;
+
+        ListView listview = (ListView) findViewById(R.id.listView) ;
+        listview.setAdapter(adapter) ;
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, IslandList.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
